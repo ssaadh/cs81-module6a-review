8a7f16bafc5102f8e4c85231aac758e3748d3841
Chase HMTV <ssgear456@gmail.com>
2026-07-09 14:50:01 -0400

    Comments for the constructor and first 2 simpler functions

---
+++ playlist.js
@@ -0,0 +1,46 @@
+// This is a constructor function that can later be used to instantiate new Playlist objects.
+// Basically it's like `constructor` function for classes.
+// `this` refers to the new object being created
+function Playlist(name) {
+  this.name = name;
+  this.songs = [];
+  this.currentSong = null;
+}
+
+// basic function pushing a songTitle to the songs array
+Playlist.prototype.addSong = function(songTitle) {
+  this.songs.push(songTitle);
+};
+
+// if there are songs then the first song in the songs array becomes the currentSong
+// Then a console log of the variable
+Playlist.prototype.playFirst = function() {
+  if (this.songs.length > 0) {
+    this.currentSong = this.songs[0];
+    console.log("Now playing:", this.currentSong);
+  }
+};
+
+Playlist.prototype.skipSong = function() {
+  if (this.songs.length > 1) {
+    this.songs.shift();
+    this.currentSong = this.songs[0];
+    console.log("Skipped! Now playing:", this.currentSong);
+  } else {
+    console.log("No more songs to skip.");
+  }
+};
+
+Playlist.prototype.listSongs = function() {
+  console.log("Playlist:", this.name);
+  console.log("Songs:", this.songs.join(", "));
+};
+
+
+let myMix = new Playlist("My Chill Mix");
+myMix.addSong("Lofi Study");
+myMix.addSong("Chillhop Beats");
+myMix.addSong("Evening Jazz");
+myMix.playFirst();
+myMix.skipSong();
+myMix.listSongs();
